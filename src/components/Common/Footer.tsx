1c4b634e05f6fd79ac54215cfb50a965a8259fdf
ChangHyun Park <buschanny@gmail.com>
2024-09-24 20:27:02 +0900

    Initial Commit

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react'
+import styled from '@emotion/styled'
+
+const FooterWrapper = styled.footer`
+  display: grid;
+  place-items: center;
+  margin-top: auto;
+  padding: 50px 0;
+  font-size: 15px;
+  text-align: center;
+  line-height: 1.5;
+
+  @media (max-width: 768px) {
+    font-size: 13px;
+  }
+`
+
+const Footer: FC = () => {
+  return (
+    <FooterWrapper>
+      Thank You for Visition My Blog, Have a Good Day 😊
+      <br />© 2024 Developer Daven, Powered By Gatsby.
+    </FooterWrapper>
+  )
+}
+
+export default Footer
